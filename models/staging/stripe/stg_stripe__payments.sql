28c2e3089902da3cdfaf1f33ef9e911c799d9a4e
Nick Neal <nick@thedatadriven.co>
2026-09-16 01:31:16 +0000

    fixed stg_stripe__payments to reflect name changes in course

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -5,10 +5,10 @@ renamed as (
         id as payment_id,
         orderid as order_id,
         paymentmethod as payment_method,
-        status,
-        amount / 100 as amount,
-        created as created_at,
-        _batched_at as batched_at
+        status as payment_status,
+        amount / 100 as payment_amount,
+        created as payment_created_at,
+        _batched_at
     
     from {{ source('stripe', 'payment') }}
 
