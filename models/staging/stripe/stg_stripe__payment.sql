05c68bcd76ef447d14f2eaa9b2424e835ccb3271
Nick Neal <nick@thedatadriven.co>
2026-09-17 01:58:47 +0000

    fix typo

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -9,7 +9,7 @@ source as (
 renamed as (
 
     select
-        id as payment_id
+        id as payment_id,
         orderid as order_id,
         paymentmethod as payment_method,
         status as payment_status,
